4ea2c3238d318a4f30a6fc6b1bd14a6c82d9826e
a-png129 <alice.peng129@gmail.com>
2025-08-14 00:07:38 -0700

    comments clean up

--- src/public/scripts.js
+++ src/public/scripts.js
@@ -1,17 +1,3 @@
-/*
- * These functions below are for various webpage functionalities. 
- * Each function serves to process data on the frontend:
- *      - Before sending requests to the backend.
- *      - After receiving responses from the backend.
- * 
- * To tailor them to your specific needs,
- * adjust or expand these functions to match both your 
- *   backend endpoints 
- * and 
- *   HTML structure.
- * 
- */
-
 
 // This function checks the database connection and updates its status on the frontend.
 async function checkDbConnection() {
